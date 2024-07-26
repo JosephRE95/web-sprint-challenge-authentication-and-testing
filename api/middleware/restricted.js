1bc877ec8a5840a74807f4b4d8606853fb68d85b
Joseph Ellis <josephellis95@gmail.com>
2024-07-26 16:14:26 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
       next();
     } catch (e) {
       console.log(e)
-      res.send("token invalid");
+      res.status(401).send("token invalid");
     }
   }
 };

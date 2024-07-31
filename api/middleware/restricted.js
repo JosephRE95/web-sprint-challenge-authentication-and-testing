637d665bd1ff5c5d486015f33908f7721c64afa1
Joseph Ellis <josephellis95@gmail.com>
2024-07-31 15:33:28 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -3,14 +3,14 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   console.log(token)
-  if (!token) res.status(400).send("token required");
+  if (!token) res.status(400).json("token required");
   else {
     try {
       jwt.verify(token, process.env.JWT_SECRET);
       next();
     } catch (e) {
       console.log(e)
-      res.status(401).send("token invalid");
+      res.status(401).json("token invalid");
     }
   }
 };
